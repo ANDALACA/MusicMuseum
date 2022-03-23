ed5fbea1fecf247a417dba894adffaa396393974
CarlHutters <carlhuttersch@gmail.com>
2022-03-23 15:50:46 +0100

    alt virker (måske)

--- Music Museum/Assets/Scripts/ButtonScript.cs
+++ Music Museum/Assets/Scripts/ButtonScript.cs
@@ -6,36 +6,24 @@ using UnityEngine.Events;
 public class ButtonScript : MonoBehaviour
 {
     public UnityEvent buttonClicked;
-    private Vector3 position;
     [ReadOnly] public AudioSource audioSource;
     private bool pressCheckBool = false;
     [ReadOnly] public bool instrumentPlayingCheckBool = false;
     void Start()
     {
-        position = transform.position;
         audioSource = GetComponent<AudioSource>();
     }
 
-    void Update()
+    private void OnTriggerEnter(Collider col)
     {
-        if (transform.position.y < position.y - transform.lossyScale.y)
+        if(col.tag == "Hand")
         {
-            transform.position = new Vector3(transform.position.x, position.y - transform.lossyScale.y, transform.position.z);
-            
-            if(!pressCheckBool && !instrumentPlayingCheckBool)
+            GetComponent<Animation>().Play();
+            if(!instrumentPlayingCheckBool)
             {
                 audioSource.Play();
-                pressCheckBool = true;
                 buttonClicked.Invoke();
             }
         }
-
-        if (transform.position.y == position.y)
-        {
-            pressCheckBool = false;
-        }
-
-        if (transform.position.y > position.y)
-            transform.position = position;
     }
 }
--- Music Museum/Assets/Scripts/HapticImpulse.cs
+++ Music Museum/Assets/Scripts/HapticImpulse.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using UnityEngine.XR;
 
 public class HapticImpulse : MonoBehaviour
-{    
+{
     public void TriggerHaptics(InputDevice inputDevice, float amplitude, float duration)
     {
-        inputDevice.SendHapticImpulse(1, amplitude, duration);
+        inputDevice.SendHapticImpulse(0, amplitude, duration);
     }
 }
